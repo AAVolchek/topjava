86d24c050d59ec104d062e0e78c0b675783e943f
AAVolchek <71441342+AAVolchek@users.noreply.github.com>
2022-04-21 13:53:05 +0200

    Apply 11_12_XSS.patch

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepository.java
@@ -10,7 +10,7 @@ import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
-import ru.javawebinar.topjava.util.ValidationUtil;
+import ru.javawebinar.topjava.util.validation.ValidationUtil;
 
 import java.time.LocalDateTime;
 import java.util.List;
